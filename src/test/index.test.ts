a89265803ddc85a3d06ab1d7ccf9a51555a7ac26
Tom <tomherni@gmail.com>
2025-04-21 21:30:51 +0200

    feat: add convertMarkdown() to convert quotes in Markdown text (#6)

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -2,10 +2,16 @@ import * as assert from 'node:assert/strict';
 import { describe, test } from 'node:test';
 import * as pkg from '../index.js';
 import { convert } from '../convert.js';
+import { convertMarkdown } from '../convert-markdown.js';
 
 describe('API', () => {
   test('exports convert()', () => {
     assert.equal(typeof pkg.convert, 'function');
     assert.equal(pkg.convert, convert);
+  });
+
+  test('exports convertMarkdown()', () => {
+    assert.equal(typeof pkg.convertMarkdown, 'function');
+    assert.equal(pkg.convertMarkdown, convertMarkdown);
   });
 });
