eb5a58b8a3b69f40e2d47a7afd65cfd7ad799f59
Tom <tomherni@gmail.com>
2026-06-18 21:28:16 +0200

    chore: update dev dependencies (#10)

--- eslint.config.js
+++ eslint.config.js
@@ -1,4 +1,18 @@
-/** @type {import('eslint').Linter.Config[]} */
-const config = [{}];
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import { defineConfig } from 'eslint/config';
 
-export default config;
+export default defineConfig([
+  {
+    files: ['**/*.{js,ts}'],
+    plugins: { js },
+    extends: ['js/recommended'],
+    languageOptions: {
+      globals: globals.node,
+      ecmaVersion: 2022,
+      sourceType: 'module',
+    },
+  },
+  tseslint.configs.recommended,
+]);
